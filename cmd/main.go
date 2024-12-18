11ac9af616b47f2e3bbbac825c20e7641658dd6e
a-korkin <a-korkin88@yandex.ru>
2024-12-18 06:54:44 +0300

    fix: creating dir if not exists

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/a-korkin/csvparser/internals/tools"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	fileName := "internals/data/ex.csv"
+	dirName := "data"
+	fileName := "ex.csv"
+	err := os.MkdirAll(dirName, os.ModePerm)
+	if err != nil {
+		log.Fatalf("failed to create dir: %s", err)
+	}
+
+	filePath := filepath.Join(dirName, fileName)
+
 	link := "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional-size-bands.csv"
-	tools.DownloadFile(link, fileName)
-	tools.ParseFile(fileName)
+	tools.DownloadFile(link, filePath)
+	tools.ParseFile(filePath)
 }
